178ea5f055e7cda721b7d75c0d303039f74286e5
Alina <alinavalentinaiacob@gmail.com>
2026-01-06 22:15:24 +0200

    eda for product_info.csv

--- eda.py
+++ eda.py
@@ -1,12 +1,13 @@
+from array import array
+
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+from utils import sum_nan_values,histogramVariable
 products_df = pd.read_csv("./dataset/product_info.csv")
 
 
-columns = products_df.columns.tolist()
-print(columns)
+
 num_prod = len(products_df['product_id'].unique())
 print("num prod", num_prod)
 num_brands = len(products_df["brand_id"].unique())
@@ -15,9 +16,38 @@ print("num of products", products_df['product_id'].unique())
 #columns used for analysis
 products_df = products_df.drop(columns=['product_id','brand_id'])
 print("products_df after drop cols\n",products_df)
+columns = products_df.columns.tolist()
+print(columns)
 print("cols ",products_df.columns.tolist())
 
 print(products_df.describe())
-
+statistics = products_df.describe()
+print("type of products_df", type(statistics))
+print("res for ", statistics["loves_count"])
+numeric_columns = statistics.columns.tolist()
+print("numeric cols", numeric_columns)
 st.title("Beauty Reviews Analyzer App")
-st.text(f"We analyzed {num_prod} products from ${num_brands}. We aim ")
+st.text(f"We analyzed {num_prod} products from {num_brands}. We aim ")
+st.text(f"The columns used for this research {columns}")
+
+option = st.selectbox(
+    "Choose a column to analyze",
+    numeric_columns
+)
+st.write("You select", option)
+st.write("Statistics for your option ", statistics[option])
+
+
+sum_nan = sum_nan_values(products_df)
+print(sum_nan)
+
+null_percentage = (sum_nan/len(products_df))*100
+print("null percentage\n ", null_percentage)
+
+
+st.text(f"Null percentage for variables {null_percentage}")
+histogramVariable(products_df, option)
+
+non_numeric_cols = products_df.select_dtypes(include=[object]).columns.tolist()
+for non_num_col in non_numeric_cols:
+    print(f"for col {non_num_col}",set(products_df[non_num_col].values.tolist()))
