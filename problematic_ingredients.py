a4d2aa7f85d4e7596f73ed4037b2c12c1865c0e9
Alina <alinavalentinaiacob@gmail.com>
2026-01-07 16:57:28 +0200

    problematic ingredients

---
+++ problematic_ingredients.py
@@ -0,0 +1,33 @@
+PROBLEMATIC_ALCOHOLS = {
+    "alcohol denat",
+    "sd alcohol",
+    "sd alcohol 39c",
+    "ethanol",
+    "isopropyl alcohol"
+}
+
+FRAGRANCE_ALLERGENS = {
+    "parfum",
+    "fragrance",
+    "linalool",
+    "limonene",
+    "citral",
+    "geraniol",
+    "eugenol",
+    "farnesol",
+    "coumarin",
+    "benzyl alcohol",
+    "benzyl benzoate",
+    "benzyl salicylate",
+    "hexyl cinnamal",
+    "alpha-isomethyl ionone",
+    "isoeugenol",
+    "cinnamal"
+}
+CONTROVERSIAL_FILTERS = {
+    "ethylhexyl methoxycinnamate",
+    "octinoxate",
+    "butyl methoxydibenzoylmethane",
+    "avobenzone",
+    "ethylhexyl salicylate"
+}
