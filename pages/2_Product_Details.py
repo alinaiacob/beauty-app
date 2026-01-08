3a82274c6f0cd02804793fe52b5d03c0fcb97b09
Alina <alinavalentinaiacob@gmail.com>
2026-01-08 20:06:26 +0200

    fetch product reviews

--- pages/2_Product_Details.py
+++ pages/2_Product_Details.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from utils import load_products_data
-
+from utils import load_products_data, getStatisticsForProduct, getReviewsAboutProducts
 
 st.title("Product Details Page")
 
@@ -14,3 +13,23 @@ if not product or not brand:
     st.stop()
 
 
+df = load_products_data()
+product_df = df[
+    (df["brand_name"]==brand) &
+    (df["product_name"]==product)
+]
+
+st.write("Info for selected product", product_df)
+st.write("Reviews ", product_df['reviews'])
+st.write("Primary category", product_df["primary_category"])
+st.write("Secondary category", product_df["secondary_category"])
+st.write("Rating for this product ", product_df['rating'])
+
+mean_rating = getStatisticsForProduct(product_df)
+st.write("Mean rating from other products from same category", mean_rating)
+
+product_id = product_df["product_id"].iloc[0]
+
+reviews = getReviewsAboutProducts(product_id)
+st.write("Reviews ", reviews)
+#st.write("Reviews for products ", reviews["review_text"])
